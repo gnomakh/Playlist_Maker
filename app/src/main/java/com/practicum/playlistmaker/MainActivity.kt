40855732144d61d243d2b90d92bfd43e8e5e3989
Саша Аксенов <laorha@yandex.ru>
2024-06-03 16:50:42 +0500

    Добавил OnClickListener

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId") // Потом уберу
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton =  findViewById<Button>(R.id.settings_button)
+
+        val searchButtonOnClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "ТЫ НАЖАЛ ПОИСК!!!", Toast.LENGTH_SHORT).show()
+            }
         }
+        searchButton.setOnClickListener(searchButtonOnClickListener)
+
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "ТЫ НАЖАЛ МЕДИАТЕКУ!!!", Toast.LENGTH_SHORT).show()
+        }
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "ТЫ НАЖАЛ НАСТРОЙКИ!!!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
