efec4fb635291e89cb140fbdf8105b40715eaec5
Саша Аксенов <laorha@yandex.ru>
2024-07-23 17:52:15 +0500

    Реализовал поиск треков с помощью Retrofit, сделал заглушки на случай отсутствия результата поиска или проблем с сетью, добавил кнопку "Обновить"

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,14 @@
 package com.practicum.playlistmaker
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -37,6 +40,7 @@ class SearchActivity : AppCompatActivity() {
     private val trackService = retrofit.create(ITunesAPI::class.java)
 
     private lateinit var backButton: ImageView
+    private lateinit var updateButton: Button
     private lateinit var inputEditText: EditText
     private lateinit var clearButton: ImageView
     private lateinit var rvTrackList: RecyclerView
@@ -49,9 +53,10 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val inflater = LayoutInflater.from(this)
         binding = ActivitySearchBinding.inflate(inflater)
-        setContentView(R.layout.activity_search)
+        setContentView(binding.root)
 
         backButton = binding.backButton
+        updateButton = binding.updateButton
         inputEditText = binding.inputSearch
         clearButton = binding.clearButton
 
@@ -63,6 +68,7 @@ class SearchActivity : AppCompatActivity() {
             inputEditText.setText("")
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             imm?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+            clearTrackList()
         }
 
         inputEditText.doOnTextChanged { text, start, before, count ->
@@ -77,41 +83,67 @@ class SearchActivity : AppCompatActivity() {
         rvTrackList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rvTrackList.adapter = adapter
 
+        var lastSearchQueue = ""
+
         inputEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                trackService.search(binding.inputSearch.text.toString())
-                    .enqueue(object : Callback<SearchResponse> {
-                        override fun onResponse(
-                            call: Call<SearchResponse>, response: Response<SearchResponse>
-                        ) {
-                            when (response.code()) {
-                                200 -> {
-                                    tracks.clear()
-                                    if (response.body()?.results?.isNotEmpty() == true) {
-                                        tracks.addAll(response.body()?.results!!)
-                                        adapter.notifyDataSetChanged()
-                                    } else {
-                                        doNothing()
-                                    }
-                                }
-
-                                else -> {
-                                    doNothing()
-                                }
-                            }
-                        }
-
-                        override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                            doNothing()
-                        }
-
-                    })
+                lastSearchQueue = binding.inputSearch.text.toString()
+                queue(binding, adapter, trackService, tracks, lastSearchQueue)
             }
             false
         }
+
+        binding.updateButton.setOnClickListener {
+            queue(binding, adapter, trackService, tracks, lastSearchQueue)
+        }
     }
 
-    fun doNothing() {}
+    fun queue(
+        binding: ActivitySearchBinding,
+        adapter: TracksAdapter,
+        trackService: ITunesAPI,
+        tracks: ArrayList<Track>,
+        searchQueue: String
+    ): Boolean {
+        binding.searchPlaceholder.visibility = View.GONE
+        binding.networkErrorPalceholder.visibility = View.GONE
+        trackService.search(searchQueue)
+            .enqueue(object : Callback<SearchResponse> {
+                override fun onResponse(
+                    call: Call<SearchResponse>, response: Response<SearchResponse>
+                ) {
+
+                    when (response.code()) {
+                        200 -> {
+                            tracks.clear()
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                tracks.addAll(response.body()?.results!!)
+                                adapter.notifyDataSetChanged()
+                            } else {
+                                adapter.notifyDataSetChanged()
+                                binding.searchPlaceholder.visibility = View.VISIBLE
+                            }
+                        }
+
+                        else -> {
+                            clearTrackList()
+                            binding.networkErrorPalceholder.visibility = View.VISIBLE
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                    clearTrackList()
+                    binding.networkErrorPalceholder.visibility = View.VISIBLE
+                }
+            })
+        return true
+    }
+
+    private fun clearTrackList() {
+        tracks.clear()
+        adapter.notifyDataSetChanged()
+    }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
--- app/src/main/java/com/practicum/playlistmaker/TracksHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksHolder.kt
@@ -4,6 +4,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.playlistmaker.databinding.TrackItemBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 
 class TracksHolder(private val trackItemBinding: TrackItemBinding) : RecyclerView.ViewHolder(trackItemBinding.root) {
@@ -11,7 +13,7 @@ class TracksHolder(private val trackItemBinding: TrackItemBinding) : RecyclerVie
     fun bind(model: Track) {
         trackItemBinding.trackName.text = model.trackName
         trackItemBinding.artistName.text = model.artistName
-        trackItemBinding.trackDuration.text = model.trackTimeMillis
+        trackItemBinding.trackDuration.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTimeMillis)
         val artwork = trackItemBinding.artworkCover
 
         Glide.with(itemView.context).load(model.artworkUrl100).placeholder(R.drawable.placeholder).fitCenter()
